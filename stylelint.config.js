bce3d84e60a2ed2cc0a22d418692a6b3690c027c
Thomas Aull <post@thomasaull.de>
2018-12-04 17:57:57 +0100

    🔧 vue config, stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,31 @@
 module.exports = {
   root: true,
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier']
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: ['stylelint-scss', 'stylelint-selector-bem-pattern'],
+  rules: {
+    'at-rule-no-unknown': null,
+
+    'plugin/selector-bem-pattern': {
+      implicitComponents: [
+        '**/src/components/**/*',
+        '**/src/views/**/*',
+        '**/src/layouts/**/*'
+      ],
+      preset: 'suit',
+      ignoreSelectors: [
+        /^svg$/,
+        /router-link-active$/,
+        /router-link-exact-active$/,
+        /lazyloading$/,
+        /lazyloaded$/,
+        /lazyload$/,
+        /v-enter$/,
+        /v-enter-to$/,
+        /v-enter-active$/,
+        /v-leave$/,
+        /v-leave-to$/,
+        /v-leave-active$/
+      ]
+    }
+  }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,37 @@
+const svgoplugins = require('./svgoplugins')
+
 module.exports = {
   pluginOptions: {
     lintStyleOnBuild: true,
     stylelint: {}
+  },
+
+  css: {
+    sourceMap: true,
+    loaderOptions: {
+      sass: {
+        data: `
+        @import "~include-media/dist/_include-media.scss";
+        @import "~modularscale-sass/stylesheets/modularscale";
+        @import "@/assets/scss/constants.scss";
+        @import "@/assets/scss/interpolate.scss";
+        @import "@/assets/scss/easing.scss";
+        `
+      }
+    }
+  },
+
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .options({
+        svgo: {
+          plugins: svgoplugins
+        }
+      })
+      .end()
   }
 }
