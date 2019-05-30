199abcd974c77385c96486827dd288a332332743
Thomas Aull <post@thomasaull.de>
2019-05-30 17:55:08 +0200

    🚧 Configs for Urls, Constants, SCSS Constants

---
+++ src/config/urls.js
@@ -0,0 +1,7 @@
+export const GRAPHQL_URL = `${process.env.VUE_APP_API_URL}/graphql`
+export const API_URL = process.env.VUE_APP_API_URL
+
+export default {
+  GRAPHQL: GRAPHQL_URL,
+  API: API_URL
+}
--- src/main.js
+++ src/main.js
@@ -10,15 +10,21 @@ import VueWait from 'vue-wait'
 import VueMeta from 'vue-meta'
 import Icon from '@/components/Icon'
 import EventHub from '@/EventHub'
+import scssConstants from '@/assets/scss/constants.scss'
+import urls from '@/config/urls'
+import constants from '@/config/constants'
 
 Vue.config.productionTip = false
-
-Vue.prototype.$eventHub = EventHub
 
 Vue.use(Vuex)
 Vue.use(PortalVue)
 Vue.use(VueWait)
 Vue.use(VueMeta)
+
+Vue.prototype.$eventHub = EventHub
+Vue.prototype.$scssConstants = scssConstants
+Vue.prototype.$urls = urls
+Vue.prototype.$constants = constants
 
 // Global Components
 Vue.component('Icon', Icon)
@@ -31,5 +37,8 @@ const store = new Vuex.Store({ state, mutations, getters, actions })
 new Vue({
   router,
   store,
+  wait: new VueWait({
+    useVuex: true
+  }),
   render: h => h(App)
 }).$mount('#app')
