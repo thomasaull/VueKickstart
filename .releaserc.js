7a8d31c0113ae8174954cf1e559b187234bda1ad
Thomas Aull <post@thomasaull.de>
2019-11-28 10:18:25 +0100

    🔧 Update semantic release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,15 +1,31 @@
 module.exports = {
   branch: 'master',
   plugins: [
-    'semantic-release-gitmoji',
-    '@semantic-release/npm',
     [
-      '@semantic-release/git',
+      'semantic-release-gitmoji',
+      {
+        releaseRules: {
+          major: [':boom:'],
+          minor: [':sparkles:'],
+          patch: [':bug:', ':ambulance:', ':lock:']
+        }
+      }
+    ],
+
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false
+      }
+    ],
+
+    [
+      ('@semantic-release/git',
       {
         assets: ['package.json', 'package-lock.json'],
         message:
           '🔖 Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }
-    ],
+      })
+    ]
   ]
 }
