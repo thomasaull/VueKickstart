20873963cab9cf0546eba11d22997885c8f18986
thomasaull <post@thomasaull.de>
2020-07-23 14:46:06 +0200

    🔧 Update configs

--- stylelint.config.js
+++ stylelint.config.js
@@ -13,6 +13,8 @@ module.exports = {
       ],
       preset: 'suit',
       ignoreSelectors: [
+        /^#{\$block}/,
+        /^iframe$/,
         /^svg$/,
         /router-link-active$/,
         /router-link-exact-active$/,
