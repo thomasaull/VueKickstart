9b76c3c525187381e320ebaf55d439836bbbafd2
Thomas Aull <post@thomasaull.de>
2021-03-30 23:27:40 +0200

    🚧 Make vue and storybook work

--- src/components/Example.stories.ts
+++ src/components/Example.stories.ts
@@ -21,17 +21,14 @@ const meta: Meta<Args> = {
 export default meta;
 
 const Template: Story<Args> = (args) => ({
-  // Components used in your story `template` are defined in the `components` object
   components: { Example },
-  // The story's `args` need to be mapped into the template through the `setup()` method
+
   setup() {
-    // Story args can be spread into the returned object
     return {
       args,
     };
   },
 
-  // Then, the spread values can be accessed directly in the template
   template: `<Example v-bind="args">Abc</Example>`,
 });
 
